9919e6b81ca09d3bb3635dc0af5c13116f8ee0a4
Cara Ponzini <ponzini@gmail.com>
2019-03-06 15:30:06 -0800

    designed test and sample object modeling API data return format

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,41 @@
 import './html-equal.js';
+
+const test = QUnit.test;
+
+function makePatternTemplate(pattern) {
+
+}
+
+test('html template will match template literal', assert => {
+    //arrange
+    const pattern = {
+        "id": 909820,
+        "name": "Clean Lines",
+        "permalink": "clean-lines-2",
+        "first_photo": {
+            "medium_url": "https://images4-b.ravelrycache.com/uploads/stitchnerd/611130017/stitchnerd-023_medium.jpg",
+        },
+        "pattern_author": {
+            "name": "Susan Ashcroft",
+            "patterns_count": 259,
+            "permalink": "susan-ashcroft",
+            "users": [
+                {
+                    "username": "stitchnerd",
+                }
+            ]
+        },
+        "pattern_sources": [
+            {
+                "url": "http://www.ravelry.com/stores/stitchnerd-designs",
+            }
+        ]
+    };
+
+    //act
+    const result = makePatternTemplate(pattern);
+
+    //assert
+    assert.htmlEqual(result, ``);
+});
+

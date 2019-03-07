9047d06d7286b84342e4911a144bc1966295d253
Cara Ponzini <ponzini@gmail.com>
2019-03-06 16:05:02 -0800

    test passed for template literal

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,27 @@ import './html-equal.js';
 const test = QUnit.test;
 
 function makePatternTemplate(pattern) {
+    const html = `
+    <li>
+        <section class="li-text">
+            <a href="https://www.ravelry.com/patterns/library/${pattern.permalink}">
+                <h2>
+                    ${pattern.name}
+                </h2>
+            </a>   
+            <p>Designer: <a href="${pattern.pattern_sources[0].url}">${pattern.pattern_author.name}</a></p>
+            <p>Ravelry Name: ${pattern.pattern_author.users[0].username}</p>                    
+        </section>
+        <section class="li-image">
+            <a href="https://www.ravelry.com/patterns/library/${pattern.permalink}">
+                <img src="${pattern.first_photo.medium_url}" alt="${pattern.name}">
+            </a>
+        </section>
+    </li>`;
 
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
 }
 
 test('html template will match template literal', assert => {
@@ -36,6 +56,23 @@ test('html template will match template literal', assert => {
     const result = makePatternTemplate(pattern);
 
     //assert
-    assert.htmlEqual(result, ``);
+    assert.htmlEqual(result, `
+    <li>
+        <section class="li-text">
+            <a href="https://www.ravelry.com/patterns/library/clean-lines-2">
+                <h2>
+                    Clean Lines
+                </h2>
+            </a>   
+            <p>Designer: <a href="http://www.ravelry.com/stores/stitchnerd-designs">Susan Ashcroft</a></p>
+            <p>Ravelry Name: stitchnerd</p>                    
+        </section>
+        <section class="li-image">
+            <a href="https://www.ravelry.com/patterns/library/clean-lines-2">
+                <img src="https://images4-b.ravelrycache.com/uploads/stitchnerd/611130017/stitchnerd-023_medium.jpg" alt="Clean Lines">
+            </a>
+        </section>
+    </li>
+    `);
 });
 

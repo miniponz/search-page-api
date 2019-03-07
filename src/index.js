946b6733ec61e9d7fa121d42ca70890ab6b08fda
Cara Ponzini <ponzini@gmail.com>
2019-03-06 17:48:44 -0800

    created header for API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import loadPatterns from './load-patterns.js';
 import data from './sample-object.js';
 
+
+const query = encodeURIComponent('ding dong');
+const page = 1;
+const username = 'read-4c64aafe518a851fe9f72f0fc442337b';
+const password = 'l13ub39WAF6WkObCfRJ4k9D450cdrAcMWlRWfjir';
+const url = 'https://cors-anywhere.herokuapp.com/https://api.ravelry.com/patterns/search.json?query=ding+dong';
+
+fetch(url, {
+    headers: {
+        Authorization: `Basic ${Buffer.from(username + ':' + password).toString(
+            'base64'
+        )}`,
+        Origin: null
+    }
+})
+    .then(res => res.json())
+    .then(results => console.log(results));
+
 loadPatterns(data);
--- src/load-patterns.js
+++ src/load-patterns.js
@@ -1,7 +1,11 @@
 import makePatternTemplate from './make-pattern-template.js';
 const patternDisplay = document.getElementById('pattern-display');
 
-export default function loadPatterns(data) {    
+export default function loadPatterns(data) {   
+    // while(patternDisplay.children.length > 0) {
+    //     patternDisplay.lastElementChild.remove();
+    // }
+    
     data.patterns.forEach(pattern => {
         const dom = makePatternTemplate(pattern);
         patternDisplay.appendChild(dom);
